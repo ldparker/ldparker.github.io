dec306c66dcaba48ec5e2121d24ff6eb14cdc6e3
Logan Parker <logandparker4@gmail.com>
2017-02-06 22:59:16 -0800

    interests added pictures and effects

--- script.js
+++ script.js
@@ -68,6 +68,60 @@ $(document).ready(function() {
         $(this).removeClass('hoveredend');
     });
     
+    /*Cycling effects begin*/
+    
+    $('#cyclepic1, .cyclepictext').hover(function() {
+        $('#cyclepic1').addClass('cyclehover');
+    },
+    function() {
+        $('#cyclepic1').removeClass('cyclehover');
+    });
+    
+    $('#cyclepic1, .cyclepictext').hover(function() {
+        $('.cyclepictext').show();
+    },
+    function() {
+        $('.cyclepictext').hide();
+    });
+    
+    /*$('#cyclepic0, .cyclepictext2').hover(function() {
+        $('#cyclepic0').addClass('cyclehover2');
+    },
+    function() {
+        $('#cyclepic0').removeClass('cyclehover2');
+    });*/
+    
+    /*currently dont think I want hover functionality on small 1st photo*/
+    /*$('#cyclepic0, .cyclepictext2').hover(function() {
+        $('.cyclepictext2').show();
+    },
+    function() {
+        $('.cyclepictext2').hide();
+    });*/
+    /* end needs work*/
+    
+    $('.x').hover(function() {
+        $('.x').toggleClass('xhover');
+    });
+    
+    $('#cyclepic1, .cyclepictext').click(function() {
+        $('.interestsclimbcontainer, .interestsskicontainer, .interestssurfcontainer, .interestscyclecontainer').hide(0);
+        $('.morecyclepics, .x').show(0);
+    });
+    
+    $('.x').click(function() {
+        $('.morecyclepics, .x').hide(0);
+        $('.interestsclimbcontainer, .interestsskicontainer, .interestssurfcontainer, .interestscyclecontainer').show(0);
+    });
+    
+    $('.morecyclepics').hover(function() {
+        (this).css({width: "200%", height: "200%"}, 'fast');
+    });
+    
+    /* Cycling effects end*/
+    
+    
+    
     
   
 });
